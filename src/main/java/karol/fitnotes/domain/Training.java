30cadcda64fa96ea0503b5dc4351a3c6eba3f9e5
Emil <emil.wozniak.591986@gmail.com>
2020-04-11 11:40:04 +0200

    Update Training.java

--- src/main/java/karol/fitnotes/domain/Training.java
+++ src/main/java/karol/fitnotes/domain/Training.java
@@ -3,30 +3,44 @@ package karol.fitnotes.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Entity
-@NoArgsConstructor
+@Value
 @Getter
-@Setter
 @ToString
+@Indexed
+@Table(name = "training")
+@NoArgsConstructor(force = true)
 public class Training {
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
 
+    @NonNull
+    @Column(name = "training_date", nullable = false)
     @Size(min = 0, max = 20)
-    private String trainingDate;
-    @OneToMany(mappedBy = "training")
-    private List<Exercise> exercises;
+    String trainingDate; // why not Instant?
+    
+    @Singular("exercise") // you get method addExcercise()
+    @OneToMany(
+        mappedBy = "training", 
+        cascade = CascadeType.ALL, // all type of operations
+        fetch = FetchType.LAZY, // fetch on demand in repository
+        orphanRemoval = true) // you may want to remove all related reconds when instance will be deleted 
+    @JsonIgnoreProperties("training") // to prevent fetch this field in child instance
+    List<Exercise> exercises;
+    
     @ManyToOne
-    private AppUser appUser;
+    @JsonIgnoreProperties("training") // to prevent fetch this field in child instance
+    AppUser appUser;
 
-    public Training(String trainingDate) {
-        this.trainingDate = trainingDate;
-    }
+//     public Training(String trainingDate) { I don't see a reason to do this
+//         this.trainingDate = trainingDate; 
+//     } 
 }
